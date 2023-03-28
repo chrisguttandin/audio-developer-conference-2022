0bafc979a5853aa46f94134ae4144a4ad22224fc
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-28 19:29:44 +0200

    build(config): update Playwright config

--- config/playwright/config.ts
+++ config/playwright/config.ts
@@ -39,7 +39,7 @@ const config: PlaywrightTestConfig = {
     retries: env.CI ? 2 : 0,
     snapshotDir: '../../test/screenshots',
     testDir: '../../test',
-    testMatch: /(e2e|regression)\/.*\.ts/,
+    testMatch: /(e2e|regression)\/(?!.*\.po\.ts$).*\.ts$/,
     use: {
         baseURL: env.IS_SMOKE_TEST ? 'https://chrisguttandin.github.io/audio-developer-conference-2022/' : 'http://localhost:6677',
         trace: 'on-first-retry'
