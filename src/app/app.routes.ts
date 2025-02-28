a757778ac88bded81fea24deb2b572404a34a126
Christoph Guttandin <chrisguttandin@media-codings.com>
2025-02-28 14:32:17 +0100

    build(config): re-configure root route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,22 +2,17 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
     {
-        loadChildren: () => import('./slides'),
-        path: 'slides'
-    },
-    {
-        path: '',
-        pathMatch: 'prefix',
-        redirectTo: 'slides'
-    },
-    {
         children: [
             {
-                loadComponent: () => import('./slides/slide-one/slide-one.component').then(({ SlideOneComponent }) => SlideOneComponent),
-                path: ''
-            }
-        ],
-        loadComponent: () => import('./slides/slides.component').then(({ SlidesComponent }) => SlidesComponent),
-        path: ''
+                loadChildren: () => import('./slides'),
+                path: 'slides'
+            },
+            {
+                path: '**',
+                redirectTo: 'slides'
+             }
+         ],
+        path: '',
+        pathMatch: 'prefix'
     }
 ];
